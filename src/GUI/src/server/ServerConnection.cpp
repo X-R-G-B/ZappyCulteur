8ea4ad9f4e68c3847e3e18dc82b5e6a0b3100b5f
KitetsuK <brice.desousa@epitech.eu>
2023-05-23 00:51:21 +0200

    ZappyGUI: add first version of ServerConnection (need to compile)

---
+++ src/GUI/src/server/ServerConnection.cpp
@@ -0,0 +1,91 @@
+/*
+** EPITECH PROJECT, 2023
+** ZappyCulteur
+** File description:
+** ServerConnection
+*/
+
+#include <iostream>
+#include <unistd.h>
+#include <array>
+#include "ServerConnection.hpp"
+
+namespace Server {
+
+    ServerConnection::ServerConnectionException::ServerConnectionException(const std::string &message)
+        : _Msg(message) {}
+
+    const char *ServerConnection::ServerConnectionException::what() const noexcept
+    {
+        return (_Msg.c_str());
+    }
+
+    const std::string &ServerConnection::ServerConnectionException::callWhat() const noexcept
+    {
+        return (std::string(what()));
+    }
+
+    ServerConnection::ServerConnection(const std::string &port, const std::string &ip)
+        : _Port(port), _Ip(ip)
+    {
+        _Socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (_Socket < 0) {
+            throw ServerConnectionException("Socket initialization failed");
+        }
+        _Addr.sin_port = htons(std::stoul(port));
+        _Addr.sin_family = AF_INET;
+        _Addr.sin_addr.s_addr = INADDR_ANY;
+        if (connect(_Socket, (struct sockaddr *)&_Addr, sizeof(struct sockaddr))) {
+            throw ServerConnectionException("Connection failed");
+        }
+    }
+
+    ServerConnection::~ServerConnection()
+    {
+        if (_Socket > 0) {
+            close(_Socket);
+        }
+    }
+
+    void ServerConnection::sendCommand(const std::string &command)
+    {
+        write(_Socket, command.c_str(), command.size());
+    }
+
+    bool ServerConnection::isConnected()
+    {
+        return (_Socket);
+    }
+
+    void ServerConnection::receive()
+    {
+        int bytes = 0;
+
+        bytes = read(_Socket, _Buffer.data(), BUFFER_SIZE);
+        if (bytes < 0) {
+            throw ServerConnectionException("Failed to read");
+        } else if (bytes == 0) {
+            close(_Socket);
+            _Socket = 0;
+        }
+    }
+
+    void ServerConnection::update()
+    {
+        FD_ZERO(&_Rfds);
+        FD_ZERO(&_Wfds);
+        FD_ZERO(&_Efds);
+        FD_SET(_Socket, &_Rfds);
+        FD_SET(_Socket, &_Wfds);
+        FD_SET(_Socket, &_Efds);
+        if (select(_Socket + 1, &_Rfds, &_Wfds, &_Efds, 0) < 0) {
+            throw ServerConnectionException("Failed to select");
+        }
+        if (FD_ISSET(_Socket, &_Efds)) {
+            throw ServerConnectionException("Error detected");
+        }
+        if (FD_ISSET(_Socket, &_Rfds)) {
+            receive();
+        }
+    }
+}
---
+++ src/GUI/src/server/ServerConnection.hpp
@@ -0,0 +1,59 @@
+/*
+** EPITECH PROJECT, 2023
+** ZappyCulteur
+** File description:
+** ServerConnection
+*/
+
+#pragma once
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <string>
+#include <vector>
+
+namespace Server {
+
+    static constexpr std::size_t BUFFER_SIZE = 2028;
+
+    class ServerConnection {
+        public:
+
+            class ServerConnectionException : public std::exception {
+                public:
+                    explicit ServerConnectionException(const std::string &message);
+
+                    const std::string &callWhat() const noexcept;
+                private:
+                    const char *what() const noexcept override;
+
+                    std::string _Msg;
+            };
+
+            ServerConnection(const std::string& port, const std::string& ip);
+            ~ServerConnection();
+
+            void sendCommand(const std::string &command);
+
+            bool isConnected();
+
+            const std::array<char, BUFFER_SIZE> &getResponse();
+
+            void update();
+
+        protected:
+        private:
+            void receive();
+
+            std::string _Port;
+            std::string _Ip;
+            std::vector<std::string> _Tosend;
+            std::array<char, BUFFER_SIZE> _Buffer;
+            int _Socket;
+            fd_set _Rfds;
+            fd_set _Wfds;
+            fd_set _Efds;
+            sockaddr_in _Addr;
+    };
+}
