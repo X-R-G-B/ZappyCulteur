1245b788ca4e085e7d4ac086a424258b569275b8
StEgo2103 <lucadeltort23@gmail.com>
2023-05-26 16:06:29 +0200

    IA Archi Client: new base archi cli

--- src/AI/zappy_ia/Client.py
+++ src/AI/zappy_ia/Client.py
@@ -7,30 +7,16 @@ import sys
 import argparse
 
 class Client:
-    def __init__(self):
-        self.personnality = CoreIA.IAPersonnality()
+    def __init__(self, port: int, machineName: str = "localhost"):
         self.argParse = Argparse(description="Zappy AI", add_help=False)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_ip = "127.0.0.1"
-        self.nameMachine = ""
+        self.messToSend = "\n"
+        self.messReceived = "\n"
+        self.nameMachine = machineName
         self.isConnected = False
-        self.port = 0
+        self.port = port
         
-    def setPersonnality(self, perso: CoreIA.IAPersonnality):
-        self.personnality = perso
-    
-    def parseArgs(self):
-        self.argParse.add_argument("-p", type=int, help="port number", required=True)
-        self.argParse.add_argument("-n", type=str, help="name of the team", required=True)
-        self.argParse.add_argument("-h", type=str, help="name of the machine", default="localhost")
-        self.argParse.add_argument("-help", action="help", help="show this help message and exit")
-        args = self.argParse.parse_args()
-        self.port = args.p
-        self.personnality.teamName = args.n
-        self.machineName = args.h
-        return self.connectToServer()
-    
-    def connectToServer(self):
         try:
             self.client_socket.connect((self.machineName, self.port))
             print("Connected to server")
@@ -45,13 +31,13 @@ class Client:
                     if (socket == self.client_socket):
                         message = socket.recv(2048).decode()
                         if message:
-                            self.personnality.output(message)
+                            self.messReceived = message
                         else:
                             self.client_socket.close()
                             self.isConnected = False
                 for socket in write_sockets:
                     if (socket == self.client_socket):
-                        message = self.personnality.input()
+                        message = self.messToSend
                         if (message != '\n'):
                             print("Send: " + message, end='')
                             self.client_socket.sendall(message.encode())
@@ -61,13 +47,29 @@ class Client:
             print("Disconnected from server")
         finally:
             self.client_socket.close()
+            
+    def input(self, message: str):
+        self.messToSend = message
+
+    def output(self) -> str:
+        if (self.messReceived != ""):
+            print(self.messReceived)
+            return self.messReceived
+        return ""
+    
+    def parseArgs(self):
+        self.argParse.add_argument("-p", type=int, help="port number", required=True)
+        self.argParse.add_argument("-n", type=str, help="name of the team", required=True)
+        self.argParse.add_argument("-h", type=str, help="name of the machine", default="localhost")
+        self.argParse.add_argument("-help", action="help", help="show this help message and exit")
+        args = self.argParse.parse_args()
+        self.port = args.p
+        self.personnality.teamName = args.n
+        self.machineName = args.h
+        return self.connectToServer()
         
 class Argparse(argparse.ArgumentParser):
     def error(self, message):
         sys.stderr.write('error: %s\n' % message)
         self.print_help()
         sys.exit(84)
-
-if __name__ == '__main__':
-    iaCli = IAClient()
-    sys.exit(iaCli.parseArgs())
