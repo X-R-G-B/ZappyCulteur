8f9a18cb1c21b0078453af85b74e06e12345869b
Xavier Mitault <xavier.mitault@epitech.eu>
2023-05-27 19:38:07 +0200

    ZappyServer: start implementing the ai part that exec command where freq
    is 0

---
+++ src/SERVER/src/trantorien/trantorian_reduce_freq.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2023
+** zappy server trantorien
+** File description:
+** reduce freq of action
+*/
+
+#include <stddef.h>
+#include "ntw.h"
+#include "zappy.h"
+#include "trantorien.h"
+#include "internal.h"
+
+static void trantorian_need_update(trantorien_t *trantorien, zappy_t *zappy,
+    ntw_client_t *cl, action_t *action)
+{
+    // TODO
+}
+
+void trantorien_reduce_freq(trantorien_t *trantorien, zappy_t *zappy,
+    ntw_client_t *cl)
+{
+    if (trantorien == NULL) {
+        return;
+    }
+    for (int i = 0; i < NB_PARALLEL_ACTION; i++) {
+        if (trantorien->actions[i] == NULL) {
+            continue;
+        }
+        trantorien->actions[i]->freq--;
+        if (trantorien->actions[i]->freq == 0) {
+            trantorian_need_update(trantorien, zappy, cl,
+                trantorien->actions[i]);
+            action_destroy(trantorien->actions[i]);
+            trantorien->actions[i] = NULL;
+        }
+    }
+}
