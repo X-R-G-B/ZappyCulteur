69060d1f22d67ed61aec0ef031fce59076654d59
tenshi <tenshii.tv@gmail.com>
2023-05-28 23:18:57 +0200

    update cmakes

--- src/GUI/Dependencies/build-SFML.cmake
+++ src/GUI/Dependencies/build-SFML.cmake
@@ -5,6 +5,8 @@ if (WIN32)
     set(SFML_BUILD_DOC OFF)
     set(SFML_BUILD_EXAMPLES OFF)
     set(SFML_BUILD_TEST_SUITE OFF)
+    set(BUILD_SHARED_LIBS FALSE)
+    set(SFML_USE_STATIC_STD_LIBS TRUE)
     FetchContent_Declare(SFML
         GIT_REPOSITORY https://github.com/SFML/SFML.git
         GIT_TAG 2.6.x)
