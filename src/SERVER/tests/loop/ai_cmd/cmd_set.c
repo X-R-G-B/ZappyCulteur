807d0b9cc8fa869ff4063c297a72ae300efc92f1
Xavier Mitault <xavier.mitault@epitech.eu>
2023-06-05 17:53:17 +0200

    ZappyServer: add test for set object + broadcast to graphic

--- src/SERVER/tests/loop/ai_cmd/cmd_set.c
+++ src/SERVER/tests/loop/ai_cmd/cmd_set.c
@@ -60,41 +60,13 @@ static void set_up_tests(zappy_t **zappy, int nb_client, int port,
     }
 }
 
-Test(loop_cmd_ai_take, basic)
+Test(loop_cmd_ai_set, basic)
 {
     zappy_t *zappy = NULL;
     int last = 0;
     ntw_client_t *graph = NULL;
 
-    set_up_tests(&zappy, 1, 8081, &graph);
-    ntw_client_t *client = L_DATA(zappy->ntw->clients->start);
-    cr_assert_not_null(client);
-    zappy->map->tiles[0].ressources[FOOD] = 1;
-    client_t *c = L_DATA(client);
-    cr_assert_not_null(c);
-    c->cl.ai.trantorien->x = 0;
-    c->cl.ai.trantorien->y = 0;
-    last = c->cl.ai.trantorien->ressources[FOOD];
-    circular_buffer_write(client->read_from_outside, "Take food\n");
-    while (circular_buffer_is_read_ready(client->write_to_outside) == false) {
-        cr_assert_eq(loop(zappy, true), false);
-    }
-    cr_assert_str_eq(circular_buffer_read(client->write_to_outside), "ok\n");
-    cr_assert_eq(c->cl.ai.trantorien->ressources[FOOD], last + 1);
-    char *tmp = circular_buffer_read(graph->write_to_outside);
-    char buff[512] = {0};
-    snprintf(buff, sizeof(buff), "pgt %d %d\n", c->cl.ai.trantorien->id, FOOD);
-    cr_assert_str_eq(tmp, buff);
-    cr_assert_eq(zappy->map->tiles[0].ressources[FOOD], 0);
-}
-
-Test(loop_cmd_ai_take, no_food)
-{
-    zappy_t *zappy = NULL;
-    int last = 0;
-    ntw_client_t *graph = NULL;
-
-    set_up_tests(&zappy, 1, 8082, &graph);
+    set_up_tests(&zappy, 1, 8083, &graph);
     ntw_client_t *client = L_DATA(zappy->ntw->clients->start);
     cr_assert_not_null(client);
     zappy->map->tiles[0].ressources[FOOD] = 0;
@@ -102,13 +74,41 @@ Test(loop_cmd_ai_take, no_food)
     cr_assert_not_null(c);
     c->cl.ai.trantorien->x = 0;
     c->cl.ai.trantorien->y = 0;
+    c->cl.ai.trantorien->ressources[FOOD] += 1;
     last = c->cl.ai.trantorien->ressources[FOOD];
+    circular_buffer_write(client->read_from_outside, "Set food\n");
+    while (circular_buffer_is_read_ready(client->write_to_outside) == false) {
+        cr_assert_eq(loop(zappy, true), false);
+    }
+    cr_assert_str_eq(circular_buffer_read(client->write_to_outside), "ok\n");
+    cr_assert_eq(c->cl.ai.trantorien->ressources[FOOD], last - 1);
+    cr_assert_eq(zappy->map->tiles[0].ressources[FOOD], 1);
+    char *tmp = circular_buffer_read(graph->write_to_outside);
+    char buff[512] = {0};
+    snprintf(buff, sizeof(buff), "pdr %d %d\n", c->cl.ai.trantorien->id, FOOD);
+    cr_assert_str_eq(tmp, buff);
+}
+
+Test(loop_cmd_ai_set, no_food)
+{
+    zappy_t *zappy = NULL;
+    ntw_client_t *graph = NULL;
+
+    set_up_tests(&zappy, 1, 8084, &graph);
+    ntw_client_t *client = L_DATA(zappy->ntw->clients->start);
+    cr_assert_not_null(client);
+    client_t *c = L_DATA(client);
+    cr_assert_not_null(c);
+    c->cl.ai.trantorien->x = 0;
+    c->cl.ai.trantorien->y = 0;
+    zappy->map->tiles[0].ressources[FOOD] = 0;
+    c->cl.ai.trantorien->ressources[FOOD] = 0;
     circular_buffer_write(client->read_from_outside, "Take food\n");
     while (circular_buffer_is_read_ready(client->write_to_outside) == false) {
         cr_assert_eq(loop(zappy, true), false);
     }
     cr_assert_str_eq(circular_buffer_read(client->write_to_outside), "ko\n");
-    cr_assert_eq(c->cl.ai.trantorien->ressources[FOOD], last);
+    cr_assert_eq(c->cl.ai.trantorien->ressources[FOOD], 0);
     cr_assert_eq(circular_buffer_is_read_ready(graph->write_to_outside), false);
     cr_assert_eq(zappy->map->tiles[0].ressources[FOOD], 0);
 }
