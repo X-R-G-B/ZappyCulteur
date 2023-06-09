9d7458c5ef2707c165cd07d852fb6947e5ff4c7d
tenshi <tenshii.tv@gmail.com>
2023-06-09 22:04:53 +0200

    ZappyGui: add color class

---
+++ src/GUI/src/Components/Color.cpp
@@ -0,0 +1,63 @@
+/*
+** EPITECH PROJECT, 2023
+** ZappyCulteur
+** File description:
+** Color
+*/
+
+#include "Color.hpp"
+#include <SFML/Graphics.hpp>
+
+namespace GUI {
+    Color::Color(
+        std::uint8_t red,
+        std::uint8_t green,
+        std::uint8_t blue,
+        std::uint8_t alpha
+    ) :
+        _red(red),
+        _green(green),
+        _blue(blue),
+        _alpha(alpha)
+    {}
+
+    std::uint8_t Color::getRed() const
+    {
+        return _red;
+    }
+
+    std::uint8_t Color::getGreen() const
+    {
+        return _green;
+    }
+
+    std::uint8_t Color::getBlue() const
+    {
+        return _blue;
+    }
+
+    std::uint8_t Color::getAlpha() const
+    {
+        return _alpha;
+    }
+
+    void Color::setRed(std::uint8_t red)
+    {
+        _red = red;
+    }
+
+    void Color::setGreen(std::uint8_t green)
+    {
+        _green = green;
+    }
+
+    void Color::setBlue(std::uint8_t blue)
+    {
+        _blue = blue;
+    }
+
+    void Color::setAlpha(std::uint8_t alpha)
+    {
+        _alpha = alpha;
+    }
+}
