33dafeb1ac17573bec215253df644da4db5031fb
guillaumeAbel <guillaume.abel@epitech.eu>
2023-06-14 17:57:02 +0200

    ZappyAI: Fix useless file

--- src/AI/zappy_ia/ClientManager.py
+++
@@ -1,7 +0,0 @@
-from zappy_ia.Client import Client
-
-
-class ClientManager:
-    def __init__(self, port: int, machineName: str, teamName: str):
-        self._client: Client = Client(port, machineName)
-        self.teamName: str = teamName
