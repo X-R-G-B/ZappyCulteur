627bdee65d7aba87b820c5d766733ad564166705
Kitetsu <brice.desousa@epitech.eu>
2023-06-20 22:03:49 +0200

    ZappyGui: Add text for team name winner

--- src/GUI/src/Entities/EndScreen.cpp
+++ src/GUI/src/Entities/EndScreen.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "Components/Sprite.hpp"
+#include "Components/Text.hpp"
 #include "EndScreen.hpp"
 
 namespace GUI {
@@ -13,6 +14,7 @@ namespace GUI {
         static const std::string filePath = "src/GUI/assets/menu/BeautifulSteve.png";
         static const std::size_t endScreenLayer = 500;
         static const std::string endScreenId = "EndScreen";
+        static const std::string textId = "TeamName";
 
         EndScreen::EndScreen(const std::string &id,
                                 const Vector2F &position,
@@ -44,6 +46,8 @@ namespace GUI {
 
         void EndScreen::initEndScreenSprite()
         {
+            GUI::Color color(0, 0, 0, 255);
+
             try {
                 _texture.loadFromFile(filePath);
                 std::shared_ptr<GUI::Components::Sprite> sprite = std::make_shared<GUI::Components::Sprite>(
@@ -55,9 +59,14 @@ namespace GUI {
                     _height,
                     Components::CompType::HUDSPRITE
                 );
+                auto text = std::make_shared<GUI::Components::Text>(
+                    textId, _teamName, Vector2F(50, 50), color, 30, Components::CompType::HUDTEXT
+                );
                 _components.clear();
                 _components.push_back(sprite);
-                _entityCompType.push_back(Components::CompType::SPRITE);
+                _entityCompType.push_back(Components::CompType::HUDSPRITE);
+                _components.push_back(text);
+                _entityCompType.push_back(Components::CompType::HUDTEXT);
             } catch (std::exception &e) {
                 std::cerr << e.what() << std::endl;
             }
